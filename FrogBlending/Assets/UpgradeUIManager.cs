88a81d54cd59b17ff7dcacb78d6e1ded3570c7dd
LouisPerry4281 <louisperry4281@gmail.com>
2025-02-06 13:22:27 +0000

    Fixed OOB Indexing
    Fixed the upgrade menu running into out of bounds errors and now displays max level.

--- FrogBlending/Assets/UpgradeUIManager.cs
+++ FrogBlending/Assets/UpgradeUIManager.cs
@@ -23,8 +23,36 @@ public class UpgradeUIManager : MonoBehaviour
     private void UpdateUIButtons()
     {
         //Look, this whole thing is pretty damn bad, but for the sake of fast implementation, it's fine for now.
-        upgradeButtonText[0].text = "Level " + (upgradeSystem.spawnRateUpgradeLevel + 1) + " : Cost " + upgradeSystem.spawnRateUpgradeCost[upgradeSystem.spawnRateUpgradeLevel]; //Spawn Rate Upgrade
-        upgradeButtonText[1].text = "Level " + (upgradeSystem.blenderDamageUpgradeLevel + 1) + " : Cost " + upgradeSystem.blenderDamageUpgradeCost[upgradeSystem.blenderDamageUpgradeLevel]; //Blender Damage Upgrade
-        upgradeButtonText[2].text = "Level " + (upgradeSystem.blenderAttackRechargeUpgradeLevel + 1) + " : Cost " + upgradeSystem.blenderAttackRechargeCost[upgradeSystem.blenderAttackRechargeUpgradeLevel]; //Blender Attack Rate Upgrade
+
+        //SPAWN RATE
+        if (upgradeSystem.spawnRateUpgradeLevel < upgradeSystem.spawnRateUpgrade.Length - 1)
+        {
+            upgradeButtonText[0].text = "Level " + (upgradeSystem.spawnRateUpgradeLevel + 1) + " : Cost " + upgradeSystem.spawnRateUpgradeCost[upgradeSystem.spawnRateUpgradeLevel]; //Spawn Rate Upgrade
+        }
+        else
+        {
+            upgradeButtonText[0].text = "Max Level";
+        }
+
+        //BLENDER DAMAGE
+        if (upgradeSystem.blenderDamageUpgradeLevel < upgradeSystem.blenderDamageUpgrade.Length - 1)
+        {
+            upgradeButtonText[1].text = "Level " + (upgradeSystem.blenderDamageUpgradeLevel + 1) + " : Cost " + upgradeSystem.blenderDamageUpgradeCost[upgradeSystem.blenderDamageUpgradeLevel]; //Blender Damage Upgrade
+        }
+        else
+        {
+            upgradeButtonText[1].text = "Max Level";
+        }
+
+        //BLENDER ATTACK RATE
+        if (upgradeSystem.blenderAttackRechargeUpgradeLevel < upgradeSystem.blenderAttackRechargeUpgrade.Length - 1)
+        {
+            upgradeButtonText[2].text = "Level " + (upgradeSystem.blenderAttackRechargeUpgradeLevel + 1) + " : Cost " + upgradeSystem.blenderAttackRechargeCost[upgradeSystem.blenderAttackRechargeUpgradeLevel]; //Blender Attack Rate Upgrade
+        }
+        else
+        {
+            upgradeButtonText[2].text = "Max Level";
+        }
+
     }
 }
